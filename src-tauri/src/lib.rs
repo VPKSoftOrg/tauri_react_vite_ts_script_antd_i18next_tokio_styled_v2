7b8e9e710c9307158804657d3c15ef5e1c3d6cf6
VPKSoft <vpksoft@vpksoft.net>
2024-07-28 13:57:55 +0300

    Initial changes to add Android and iOS support.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,18 +11,36 @@ fn greet(name: &str) -> String {
 #[tokio::main]
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub async fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_window_state::Builder::default().build())
-        .plugin(tauri_plugin_updater::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![
-            greet,
-            load_settings,
-            save_settings
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    #[cfg(any(target_os = "android", target_os = "ios"))]
+    {
+        tauri::Builder::default()
+            .plugin(tauri_plugin_process::init())
+            .plugin(tauri_plugin_shell::init())
+            .plugin(tauri_plugin_updater::Builder::default().build())
+            .invoke_handler(tauri::generate_handler![
+                greet,
+                load_settings,
+                save_settings
+            ])
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
+    }
+
+    #[cfg(not(any(target_os = "android", target_os = "ios")))]
+    {
+        tauri::Builder::default()
+            .plugin(tauri_plugin_process::init())
+            .plugin(tauri_plugin_shell::init())
+            .plugin(tauri_plugin_updater::Builder::default().build())
+            .plugin(tauri_plugin_window_state::Builder::default().build())
+            .invoke_handler(tauri::generate_handler![
+                greet,
+                load_settings,
+                save_settings
+            ])
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
+    }
 }
 
 /// Loads the application settings requested by the frontend.
