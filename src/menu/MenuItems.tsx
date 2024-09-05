cb1cca11f1fdcfe43f8ec4760429f2e96c82f099
VPKSoft <vpksoft@vpksoft.net>
2024-09-05 16:33:56 +0300

    Fix Font Awesome import.

--- src/menu/MenuItems.tsx
+++ src/menu/MenuItems.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile, faDoorOpen, faCircleQuestion, faInfo, faGear } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { LocalizeFunction } from "../localization/Localization";
 import { MenuItems } from "./AppMenu";
 
@@ -9,12 +10,12 @@ export const appMenuItems = (localize?: LocalizeFunction): MenuItems => [
     {
         key: "fileMenu",
         label: localize?.("fileMenu") ?? "File",
-        icon: <FontAwesomeIcon icon={faFile} />,
+        icon: <FontAwesomeIcon icon={faFile as IconProp} />,
         children: [
             {
                 key: "preferencesMenu",
                 label: localize?.("preferences") ?? "Preferences",
-                icon: <FontAwesomeIcon icon={faGear} />,
+                icon: <FontAwesomeIcon icon={faGear as IconProp} />,
             },
             {
                 type: "divider",
@@ -22,19 +23,19 @@ export const appMenuItems = (localize?: LocalizeFunction): MenuItems => [
             {
                 key: "exitMenu",
                 label: localize?.("exitMenu") ?? "Exit",
-                icon: <FontAwesomeIcon icon={faDoorOpen} />,
+                icon: <FontAwesomeIcon icon={faDoorOpen as IconProp} />,
             },
         ],
     },
     {
         key: "helpMenu",
         label: localize?.("helpMenu") ?? "Help",
-        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        icon: <FontAwesomeIcon icon={faCircleQuestion as IconProp} />,
         children: [
             {
                 key: "aboutMenu",
                 label: localize?.("aboutMenu") ?? "About",
-                icon: <FontAwesomeIcon icon={faInfo} />,
+                icon: <FontAwesomeIcon icon={faInfo as IconProp} />,
             },
         ],
     },
